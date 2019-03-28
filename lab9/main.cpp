dbf9685aea4ffaefba31e7096c77d378dfd36676
DatGuyDied <49004772+DatGuyDied@users.noreply.github.com>
2019-03-28 12:24:48 +0300

    Add files via upload

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -15,6 +15,19 @@
 
 using namespace std;
 
+void PrintMenu() {
+    cout << "Menu:" << endl;
+    cout << "0. Print Menu" << endl;
+		cout << "1. Add " << endl;
+		cout << "2. Pop" << endl;
+		cout << "3. Delete" << endl;
+		cout << "4. Print List" << endl;
+		cout << "5. Print Square" << endl;
+		cout << "6. Sort" << endl;
+		cout << "7. Queue menu" << endl;
+		cout << "8. Exit" << endl;
+}
+
 int main(int argc, char** argv) {
 
 	int a, b, c;
@@ -35,7 +48,7 @@ int main(int argc, char** argv) {
 		int range_side;
 		cout << "How many elements?" << endl;
 		cin >> amount_;
-		cout << "Enter side range: from 1 to" << endl;
+		cout << "Enter side range: from 1 to ";
 		cin >> range_side;
 		cout << endl;;
 		uniform_int_distribution<int> genSide(1, range_side);
@@ -85,31 +98,23 @@ int main(int argc, char** argv) {
 	};
 
 
-
+  PrintMenu();
 	while (true) {
+    cout << ':';
 		int flag = 0;
-		int flag2 = 0;
-		int amounts = 0;
 		int Fig_ = 0;
-		cout << "Menu:" << endl;
-		cout << "1. Add " << endl;
-		cout << "2. Pop" << endl;
-		cout << "3. Destructor" << endl;
-		cout << "4. Print List" << endl;
-		cout << "5. Print Square" << endl;
-		cout << "6. Sort" << endl;
-		cout << "7. Queue menu" << endl;
-		cout << "8. Exit" << endl;
 
 		cin >> flag;
-
+    if (flag == 0) {PrintMenu();}
 		if (flag == 1) {
 			cout << "How many elements?" << endl;
-			cin >> amounts;
-			for (int i = 0; i < amounts; ++i) {
+			int k;
+			cin >> k;
+			for (int i = 0; i < k; ++i) {
 				cout << "1. Push front" << endl;
 				cout << "2. Push back" << endl;
 				cout << "3. Insert" << endl;
+				int flag2 = 0;
 				cin >> flag2;
 				if (flag2 == 1) {
 					cout << "1. Rectangle" << endl;
@@ -117,13 +122,13 @@ int main(int argc, char** argv) {
 					cout << "3. Trapeze" << endl;
 					cin >> Fig;
 					if (Fig == 1) {
-						cout << "Enter side a and side b:" << endl;
+						cout << "Enter Rectangle:" << endl;
 					}
 					if (Fig == 2) {
-						cout << "Enter diag a and diag b:" << endl;
+						cout << "Enter Rhombus:" << endl;
 					}
 					if (Fig == 3) {
-						cout << "Set 2 Sizes and height:" << endl;
+						cout << "Enter Trapeze:" << endl;
 					}
 					cin >> a >> b;
 					if (Fig == 3) cin >> c;
@@ -133,19 +138,18 @@ int main(int argc, char** argv) {
 					list.push_f(move(f));
 				}
 				if (flag2 == 2) {
-					cout << "Choose Figure:" << endl;
-					cout << "1 - Rectangle" << endl;
-					cout << "2 - Rhombus" << endl;
-					cout << "3 - Trapeze:" << endl;
+					cout << "1. Rectangle" << endl;
+					cout << "2. Rhombus" << endl;
+					cout << "3. Trapeze:" << endl;
 					cin >> Fig;
 					if (Fig == 1) {
-						cout << "Set Sizes:" << endl;
+						cout << "Enter Rectangle:" << endl;
 					}
 					if (Fig == 2) {
-						cout << "Set Heights:" << endl;
+						cout << "Enter Rhombus:" << endl;
 					}
 					if (Fig == 3) {
-						cout << "Set 2 Sizes and height:" << endl;
+						cout << "Enter Trapeze:" << endl;
 					}
 					cin >> a >> b;
 					if (Fig == 3) cin >> c;
@@ -155,19 +159,18 @@ int main(int argc, char** argv) {
 					list.push_b(move(f));
 				}
 				if (flag2 == 3) {
-					cout << "Choose Figure:" << endl;
-					cout << "1 - Rectangle " << endl;
-					cout << "2 - Rhombus" << endl;
-					cout << "3 - Trapeze:" << endl;
+					cout << "1. Rectangle " << endl;
+					cout << "2. Rhombus" << endl;
+					cout << "3. Trapeze:" << endl;
 					cin >> Fig;
 					if (Fig == 1) {
-						cout << "Set Sizes:" << endl;
+						cout << "Enter Rectangle:" << endl;
 					}
 					if (Fig == 2) {
-						cout << "Set Heights:" << endl;
+						cout << "Enter Rhombus:" << endl;
 					}
 					if (Fig == 3) {
-						cout << "Set 2 Sizes and height:" << endl;
+						cout << "Enter Trapeze:" << endl;
 					}
 					cin >> a >> b;
 					if (Fig == 3) cin >> c;
@@ -176,7 +179,7 @@ int main(int argc, char** argv) {
 					if (Fig == 3) f = make_shared<Trapeze>(Trapeze(a, b, c));
 					if (Fig <= 3) {
 						int position;
-						cout << "Set Position:" << endl;
+						cout << "Enter Position:" << endl;
 						cin >> position;
 						list.ins(move(f), position);
 					}
@@ -185,7 +188,7 @@ int main(int argc, char** argv) {
 		}
 		if (flag == 2) {
 			int position;
-			cout << "Set Position:" << endl;
+			cout << "Enter Position:" << endl;
 			cin >> position;
 			list.pop(position);
 		}
@@ -193,7 +196,7 @@ int main(int argc, char** argv) {
       list.~TList();
 		}
 		if (flag == 4) {
-			cout << "Container:" << endl;
+			cout << "List:" << endl;
 			for (auto i : list) {
 				i->Print();
 				cout << " - ";
@@ -204,22 +207,21 @@ int main(int argc, char** argv) {
 			int position;
 			cout << "Which Element?" << endl;
 			cin >> position;
-			cout << "Square = " << list.GetSquare(position) << endl;
+			cout << list.GetSquare(position) << endl;
 		}
 		if (flag == 6) {
 			int sortType;
-			cout << "Choose sort type:" << endl;
-			cout << "1) Usual Sort" << endl;
-			cout << "2) Parallel Sort" << endl;
+			cout << "1. Usual Sort" << endl;
+			cout << "2. Parallel Sort" << endl;
 			cin >> sortType;
-			cout << "Sort" << endl;
+			cout << "Sort -------" << endl;
 			if (sortType == 1) {
 				list.sort();
 			}
 			else {
 				list.sort_parallel();
 			}
-			cout << "Done" << endl;
+			cout << "Done -------" << endl;
 		}
 		if (flag == 7) {
 			int command_add;
@@ -258,7 +260,7 @@ int main(int argc, char** argv) {
 		}
 		if (flag == 8) {
 			list.~TList();
-			return 0;
+			break;
 		}
 		else {}
 
